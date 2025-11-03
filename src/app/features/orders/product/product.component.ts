2392a2f79755be4967131a6956289647d47aee4c
erdincdagli35 <3rd1fbslash@gmail.com>
2025-11-03 20:02:41 +0300

    minor

--- src/app/features/orders/product/product.component.ts
+++ src/app/features/orders/product/product.component.ts
@@ -6,7 +6,7 @@ import { Product } from '../../models/product';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
