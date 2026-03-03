1b7fee268a9c6564d3b62795b0f653b5e1c0e99d
erdincdagli35 <3rd1fbslash@gmail.com>
2026-03-03 21:00:23 +0300

    order-list.component has been edited by roomNo v.2

--- src/app/core/order-management-system/order-list/order-list.component.ts
+++ src/app/core/order-management-system/order-list/order-list.component.ts
@@ -14,6 +14,7 @@ export class OrderListComponent {
 
     orders: Order[] = [];
     errorMessage = '';
+    roomFilter: string = '';
 
     newOrder: { id: number, status: string, total: number, createdAt:string, bills: Bill[], personalId: number, roomNo: string} = {
       status: '',
